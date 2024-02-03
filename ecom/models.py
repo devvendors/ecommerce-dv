86f1b74a953c925539794a73ace6ef6145f2d11b
Ayanavbha Chatterjee <117476380+aYgCOO@users.noreply.github.com>
2024-02-03 11:57:39 +0530

    Update

--- ecom/models.py
+++ ecom/models.py
@@ -5,7 +5,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_pic = models.ImageField(upload_to='profile_pic/CustomerProfilePic/', null=True, blank=True)
+    profile_pic = models.ImageField(
+        upload_to="profile_pic/CustomerProfilePic/", null=True, blank=True
+    )
     address = models.CharField(max_length=40)
     mobile = models.CharField(max_length=20, null=False)
     email = models.EmailField(default="abc@example.com")
@@ -24,9 +26,9 @@ class Customer(models.Model):
 
 class Product(models.Model):
     name = models.CharField(max_length=40)
-    product_image = models.ImageField(upload_to='product_image/', null=True, blank=True)
+    product_image = models.ImageField(upload_to="product_image/", null=True, blank=True)
     price = models.PositiveIntegerField()
-    description = models.CharField(max_length=40)
+    description = models.CharField(max_length=4000)
     quantity = models.IntegerField(default=1)
 
     def __str__(self):
@@ -35,13 +37,13 @@ class Product(models.Model):
 
 class Orders(models.Model):
     STATUS = (
-        ('Pending', 'Pending'),
-        ('Order Confirmed', 'Order Confirmed'),
-        ('Out for Delivery', 'Out for Delivery'),
-        ('Delivered', 'Delivered'),
+        ("Pending", "Pending"),
+        ("Order Confirmed", "Order Confirmed"),
+        ("Out for Delivery", "Out for Delivery"),
+        ("Delivered", "Delivered"),
     )
-    customer = models.ForeignKey('Customer', on_delete=models.CASCADE, null=True)
-    product = models.ForeignKey('Product', on_delete=models.CASCADE, null=True)
+    customer = models.ForeignKey("Customer", on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey("Product", on_delete=models.CASCADE, null=True)
     email = models.CharField(max_length=50, null=True)
     address = models.CharField(max_length=500, null=True)
     mobile = models.CharField(max_length=20, null=True)
