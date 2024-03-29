b569b6cf0731d2261a288197a40b7bab63fb8d2b
Agnik Roy <Ar12agnik@gmail.com>
2024-03-29 15:15:10 +0530

    minor update

--- payment_gateway/views.py
+++ payment_gateway/views.py
@@ -9,7 +9,7 @@ def paynow(request):
     mobile = request.COOKIES['mobile']
     address=request.COOKIES['address']
     print(address)
-    client = razorpay.Client(auth=("rzp_test_AZVZr5KFEnKf6Q", "zzotNAFlkUWmflBASgcIHg9v"))
+    client = razorpay.Client(auth=("rzp_test_8LqnCVttuKmh8F", "wEAsiw6baOYd9yGoPoCGgMdp"))
 
     data = {"amount": int(amount)*100, "currency": "INR", "receipt": "order_rcptid_11"}
     payment = client.order.create(data=data)
